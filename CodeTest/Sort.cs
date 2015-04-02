5562b01285402a2af487ef41430dd6e008262e72
Anthony Darwesh <anthonydarwesh@gmail.com>
2015-04-02 14:36:21 +0300

    Added classes for Sort and SortItem

---
+++ CodeTest/Sort.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeTest
+{
+    using System.IO;
+    class Sort
+    {
+        string filePath { get; set; }
+        List<string> stringList { get; set; }
+        List<SortItem> items { get; set; }
+        public Sort(string filePath)
+        {
+            this.filePath = filePath;
+            FileInfo fi = new FileInfo(filePath);
+            //throw exception if file doesn't exist
+            if (!fi.Exists) throw new FileNotFoundException("The specified file was not found");
+            using (StreamReader sr = fi.OpenText())
+            {
+                string content = sr.ReadToEnd();
+                //split the string into an array of strings
+                string[] stringArray = content.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                //remove the quotes from the string
+                this.stringList = new List<string>();
+                foreach (string st in stringArray)
+                {
+                    this.stringList.Add(st.Replace("\"", ""));
+                }
+                //sort the list
+                this.stringList.Sort();                
+            }
+            //add the sortitems from the list
+            int index = 0;
+            foreach(string st in this.stringList)
+            {
+                SortItem si = new SortItem();
+                si.word = st;
+                si.index = index;
+                //calculate the score
+                si.setScore();
+                index++;
+            }
+        }
+    }
+}
---
+++ CodeTest/SortItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeTest
+{
+    class SortItem
+    {
+        public string word { get; set; }
+        public int score { get; set; }
+        public int index { get; set; }
+        public void setScore()
+        {
+            this.score = 0;
+            int total = 0;
+            foreach(char c in this.word)
+            {
+                int index = (int)c % 32;
+                total += index;
+            }
+            //score = position * character total
+            this.score = this.index * total;
+        }
+
+    }
+}
