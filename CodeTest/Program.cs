ab5ba61a4fb3b052d50b6c60b02df23d5f4ce008
Anthony Darwesh <anthonydarwesh@gmail.com>
2015-04-02 14:08:13 +0300

    Added file open code and read into string array

--- CodeTest/Program.cs
+++ CodeTest/Program.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace CodeTest
 {
+    using System.IO;
     class Program
     {
         static void Main(string[] args)
         {
+            FileInfo fi = new FileInfo(@"C:\Users\shum\Documents\Freelancer\Code Test\names.txt");
+            try
+            {
+                using(StreamReader st = fi.OpenText())
+                {
+                    string content = st.ReadToEnd();
+                    string[] stringArray = content.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
